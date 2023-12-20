e4527e80ab0e9dc33b2d10fdbce29878cc27a450
Alabhya Dahal <46593111+AlabhyaMe@users.noreply.github.com>
2023-12-20 08:30:59 -0800

    Add files via upload

--- Simpsons.R
+++ Simpsons.R
@@ -5,49 +5,44 @@ setwd("C:/Users/meala/Documents/R Data Work")
 
 #AreaSD
 set.seed(259)
-AreaSD_hours <- rnorm(100, mean = 90, sd = 15)
-AreaSD_Income <-  abs(AreaSD_hours * (rnorm(100, 120,10))) 
+AreaSD_hours <- rnorm(100, mean = 90, sd = 3)
+AreaSD_Income <-  0.25*abs(AreaSD_hours * (rnorm(100, 105,5))) +700
 SD <- ggplot(data.frame(AreaSD_hours, AreaSD_Income), aes(x = AreaSD_hours, y = AreaSD_Income)) + 
                   geom_point(color="darkgreen")  + labs(title = "Area SD", x = "Hours", y = "Income")+theme_classic()
 
 #AreaLA
 set.seed(259)
-AreaLA_hours <- rnorm(90, mean = 100, sd = 16)
-AreaLA_Income <-  abs(AreaLA_hours * (rnorm(90, 100,8)))
+AreaLA_hours <- rnorm(90, mean = 92, sd = 3)
+AreaLA_Income <-  0.25*abs(AreaLA_hours * (rnorm(90, 97,6))) +500
 LA<- ggplot(data.frame(AreaLA_hours, AreaLA_Income), aes(x = AreaLA_hours, y = AreaLA_Income)) + 
                   geom_point(color="darkblue")  + labs(title = "Area LA", x = "Hours", y = "Income")+theme_classic()
 
 
 #AreaB
 set.seed(259)
-AreaB_hours <- rnorm(110, mean = 120, sd = 20)
-AreaB_Income <- abs(AreaB_hours * (rnorm(110, 75,6)))
+AreaB_hours <- rnorm(110, mean = 93, sd = 3)
+AreaB_Income <- 0.25*abs(AreaB_hours * (rnorm(110, 90,6))) +100
 Berkeley <- ggplot(data.frame(AreaB_hours, AreaB_Income), aes(x = AreaB_hours, y = AreaB_Income)) + 
                   geom_point(color="darkorange")  + labs(title = "Area SF", x = "Hours", y = "Income")+theme_classic()
 
 
 #Irvine
 set.seed(259)
-AreaI_hours <- rnorm(80, mean = 170, sd = 16)
-AreaI_Income <-  abs(AreaI_hours * (rnorm(80, 50,5)))
+AreaI_hours <- rnorm(80, mean = 93, sd = 4)
+AreaI_Income <-  0.25*abs(AreaI_hours * (rnorm(80, 73,5)))
 
 Irvine <- ggplot(data.frame(AreaI_hours, AreaI_Income), aes(x = AreaI_hours, y = AreaI_Income)) + 
                   geom_point(color="darkred")  + labs(title = "Area Irvine", x = "Hours", y = "Income")+theme_classic()
 
 #Riverside
 set.seed(259)
-AreaR_hours <- rnorm(75, mean = 190, sd = 14)
-AreaR_Income <-  abs(AreaR_hours * (rnorm(65, 45,4)))
+AreaR_hours <- rnorm(75, mean = 96, sd = 3)
+AreaR_Income <-  0.25*abs(AreaR_hours * (rnorm(65, 68,4))) -300
 Riverside <- ggplot(data.frame(AreaR_hours, AreaR_Income), aes(x = AreaR_hours, y = AreaR_Income)) + 
                   geom_point(color="brown")  + labs(title = "Area Riverside", x = "Hours", y = "Income")+theme_classic()
 
 #rm(list=ls())
 #plot all the plots together
-SD
-LA
-Berkeley
-Irvine
-Riverside
 
 SD+LA + Berkeley + Irvine + Riverside + plot_layout(ncol = 2)
 
@@ -73,7 +68,7 @@ AreaR <- data.frame(Area = "AreaR",Hours= AreaR_hours,Income=AreaR_Income)
 All <- rbind(AreaSD,AreaLA,AreaB,AreaI,AreaR)
 summary(lm(data=All, Income~Hours))
 
-p <- ggplot(data=All, aes(x=Hours, y=Income)) + geom_point()+geom_point(color="pink")
+p <- ggplot(data=All, aes(x=Hours, y=Income)) + geom_point()+geom_point(color="pink")+geom_smooth(method = "lm", se = FALSE, color = "lightblue")
 p<- p + theme_classic()
 
 p1 <- ggplot(data=All, aes(x=Hours, y=Income, color=Area)) + geom_point()+geom_smooth(method = "lm", se = FALSE, color = "red")
@@ -82,7 +77,7 @@ p1<- p1 + theme_classic()
 p2 <- ggplot(data=All, aes(x=Hours, y=Income, color=Area)) + geom_point()+geom_smooth(method = "lm", se = FALSE)
 p2<- p2 + theme_classic()
 
-p+p1+p2
+p1+p2
 
 Area_reg <- lm(data=All, Income~Hours+Area)
 summary(Area_reg)
